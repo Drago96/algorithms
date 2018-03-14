c724ef9eaa23a8ba6d027393df3a5be0551d3933
Drago96 <dragproychev@gmail.com>
2018-03-14 17:24:34 +0200

    Added basic recursive and combinatoric algorithms

---
+++ RecursionSortingSearching/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RecursionSortingSearching
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Recursion.GenerateCombinationsWithRepetition(new[] { 1, 2, 3, 4, 5 }, 3);
+        }
+    }
+}
